3cf82a7a7e834ca48073ed3c44d7aa3a540fdac0
Keita Mizuochi <keitam913@yahoo.co.jp>
2021-06-19 08:14:17 +0900

    Add tests of Epic

---
+++ domain/epic.test.ts
@@ -0,0 +1,22 @@
+import { Epic } from './epic';
+describe('Epic', () => {
+        describe('comparePriority()', () => {
+                test('returns >0 then self is higher priority', () => {
+                        const a = new Epic("a", "A", 1);
+                        const b = new Epic("b", "B", 2);
+                        expect(a.comparePriority(b)).toBeGreaterThan(0);
+                });
+
+                test('returns 0 then self is same priority', () => {
+                        const a = new Epic("a", "A", 1);
+                        const b = new Epic("b", "B", 1);
+                        expect(a.comparePriority(b)).toEqual(0);
+                });
+
+                test('returns <0 then self is higher priority', () => {
+                        const a = new Epic("b", "B", 2);
+                        const b = new Epic("a", "A", 1);
+                        expect(a.comparePriority(b)).toBeLessThan(0);
+                });
+        })
+});

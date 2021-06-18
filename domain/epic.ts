8a719e110cd08ce8713c78145abd7c6d43512f1b
Keita Mizuochi <keitam913@yahoo.co.jp>
2021-06-18 09:08:15 +0900

    Define the epic object

---
+++ domain/epic.ts
@@ -0,0 +1,23 @@
+export class Epic {
+        #key: string;
+        #name: string;
+        #priority: number;
+
+        constructor(key: string, name: string, priority: number) {
+                this.#key = key;
+                this.#name = name;
+                this.#priority = priority;
+        }
+
+        get key(): string {
+                return this.#key;
+        }
+
+        get name(): string {
+                return this.#name;
+        }
+
+        get priority(): number {
+                return this.#priority;
+        }
+}

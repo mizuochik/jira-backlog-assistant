976c57c36e8d6ad29bed52b466ec6fecdc2ceee7
Keita Mizuochi <keitam913@yahoo.co.jp>
2021-06-19 08:29:16 +0900

    Fix typo

--- domain/epic.test.ts
+++ domain/epic.test.ts
@@ -1,19 +1,19 @@
 import { Epic } from './epic';
 describe('Epic', () => {
         describe('comparePriority()', () => {
-                test('returns >0 then self is higher priority', () => {
+                test('returns >0 when self is higher priority', () => {
                         const a = new Epic("a", "A", 1);
                         const b = new Epic("b", "B", 2);
                         expect(a.comparePriority(b)).toBeGreaterThan(0);
                 });
 
-                test('returns 0 then self is same priority', () => {
+                test('returns 0 when self is same priority', () => {
                         const a = new Epic("a", "A", 1);
                         const b = new Epic("b", "B", 1);
                         expect(a.comparePriority(b)).toEqual(0);
                 });
 
-                test('returns <0 then self is higher priority', () => {
+                test('returns <0 when self is higher priority', () => {
                         const a = new Epic("b", "B", 2);
                         const b = new Epic("a", "A", 1);
                         expect(a.comparePriority(b)).toBeLessThan(0);

c5ace36c64c9ee70c7b9e6d61acc77983b80f46c
Keita Mizuochi <keitam913@yahoo.co.jp>
2021-06-19 14:15:48 +0900

    Add a test case of sortBacklogItemsByEpic()

---
+++ domain/sprint_backlog.test.ts
@@ -0,0 +1,36 @@
+import { BacklogItem } from './backlog_item';
+import { SprintBacklog } from './sprint_backlog';
+import { EpicList } from './epic_list';
+import { Epic } from './epic';
+
+describe('SprintBacklog', () => {
+        describe('sortBacklogItemsByEpic()', () => {
+                let sprintBacklog: SprintBacklog;
+
+                beforeEach(() => {
+                        sprintBacklog = new SprintBacklog();
+                });
+
+                test('sort items by epic priority', () => {
+                        const a = new BacklogItem('a', 'A', 'epic-A');
+                        const b = new BacklogItem('b', 'B', 'epic-B');
+                        const c = new BacklogItem('c', 'C', 'epic-C');
+                        const epicList = new EpicList();
+                        epicList.add(new Epic('epic-A', 'A', 1));
+                        epicList.add(new Epic('epic-B', 'B', 2));
+                        epicList.add(new Epic('epic-C', 'C', 3));
+
+                        sprintBacklog.addBacklogItem(b);
+                        sprintBacklog.addBacklogItem(c);
+                        sprintBacklog.addBacklogItem(a);
+
+                        sprintBacklog.sortBacklogItemsByEpic(epicList);
+
+                        expect(sprintBacklog.backlogItems).toEqual([
+                                new BacklogItem('a', 'A', 'epic-A'),
+                                new BacklogItem('b', 'B', 'epic-B'),
+                                new BacklogItem('c', 'C', 'epic-C'),
+                        ]);
+                });
+        });
+});

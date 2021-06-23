4f14478234f889b01667304a402d6f68718d4e67
Keita Mizuochi <keitam913@yahoo.co.jp>
2021-06-24 00:36:51 +0900

    Make methods of JiraBoardAccessor async

--- use_case/jira_board_accessor.ts
+++ use_case/jira_board_accessor.ts
@@ -2,7 +2,7 @@ import { SprintBacklog } from '../domain/sprint_backlog';
 import { EpicList } from '../domain/epic_list';
 
 export interface JiraBoardAccessor {
-        getEpicList(): EpicList;
-        applySprintBacklog(sprintBacklog: SprintBacklog): void;
-        getSprintBacklog(sprintName: string): SprintBacklog;
+        getEpicList(): Promise<EpicList>;
+        getSprintBacklog(sprintName: string): Promise<SprintBacklog>;
+        applySprintBacklog(sprintBacklog: SprintBacklog): Promise<void>;
 }

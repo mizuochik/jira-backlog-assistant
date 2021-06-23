f29a692e96d2e060142402fa70c160a80c75ffff
Keita Mizuochi <keitam913@yahoo.co.jp>
2021-06-24 00:34:43 +0900

    Add JiraBoardAccessor.getEpicList()

--- use_case/jira_board_accessor.ts
+++ use_case/jira_board_accessor.ts
@@ -1,6 +1,8 @@
 import { SprintBacklog } from '../domain/sprint_backlog';
+import { EpicList } from '../domain/epic_list';
 
 export interface JiraBoardAccessor {
+        getEpicList(): EpicList;
         applySprintBacklog(sprintBacklog: SprintBacklog): void;
         getSprintBacklog(sprintName: string): SprintBacklog;
 }

c35bfda96419a84245850275a9cee682da1223a5
Keita Mizuochi <keitam913@yahoo.co.jp>
2021-06-26 10:32:55 +0900

    Implement JiraBoardAccessor.getEpicList()

--- infra/http/jira_board_accessor.ts
+++ infra/http/jira_board_accessor.ts
@@ -1,10 +1,23 @@
 import { EpicList } from '../../domain/epic_list';
 import { SprintBacklog } from '../../domain/sprint_backlog';
 import { JiraBoardAccessor as UCJiraBoardAccessor } from '../../use_case/jira_board_accessor';
+import { Epic } from '../../domain/epic';
+import { EpicResponse } from './epic_response';
 
 export class JiraBoardAccessor implements UCJiraBoardAccessor {
-        getEpicList(): Promise<EpicList> {
-                return null;
+        getEpicList(boardId: string): Promise<EpicList> {
+                return new Promise((resolve, reject) => {
+                        fetch(`/rest/agile/1.0/board/${boardId}/epic`)
+                                .then(res => res.json())
+                                .then((res: EpicResponse) => {
+                                        const el = new EpicList()
+                                        res.values.forEach((epic, i) => {
+                                                el.add(new Epic(epic.key, epic.name, i));
+                                        });
+                                        resolve(el);
+                                })
+                                .catch(reject);
+                });
         }
 
         getSprintBacklog(sprintName: string): Promise<SprintBacklog> {
@@ -15,3 +28,4 @@ export class JiraBoardAccessor implements UCJiraBoardAccessor {
                 return null;
         }
 }
+

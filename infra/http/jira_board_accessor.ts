f106d9ae18e1777fbfd6571f8f9ca96f7a62a3d0
Keita Mizuochi <keitam913@yahoo.co.jp>
2021-08-05 00:20:54 +0900

    Use await statements

--- infra/http/jira_board_accessor.ts
+++ infra/http/jira_board_accessor.ts
@@ -9,49 +9,41 @@ import { BacklogItem } from '../../domain/backlog_item';
 export class JiraBoardAccessor implements IJiraBoardAccessor {
         async getEpicList(boardId: string): Promise<EpicList> {
                 async function getEpics(startAt: number, accum: EpicValue[]): Promise<EpicValue[]> {
-                        return fetch(`/rest/agile/1.0/board/${boardId}/epic?startAt=${startAt}&done=false`)
-                                .then(res => res.json())
-                                .then((res: EpicResponse) => {
-                                        if (res.isLast) {
-                                                return accum.concat(res.values);
-                                        }
-                                        return getEpics(res.startAt + res.maxResults, accum.concat(res.values));
-                                });
+                        const res = await fetch(`/rest/agile/1.0/board/${boardId}/epic?startAt=${startAt}&done=false`);
+                        const er: EpicResponse = await res.json();
+                        if (er.isLast) {
+                                return accum.concat(er.values);
+                        }
+                        return getEpics(er.startAt + er.maxResults, accum.concat(er.values));
                 }
-                return getEpics(0, [])
-                        .then((evs: EpicValue[]) => {
-                                const el = new EpicList()
-                                evs.forEach((ev, i) => {
-                                        el.add(new Epic(ev.key, ev.name, i));
-                                });
-                                return el;
-                        });
+                const evs: EpicValue[] = await getEpics(0, []);
+                const el = new EpicList()
+                evs.forEach((ev, i) => {
+                        el.add(new Epic(ev.key, ev.name, i));
+                });
+                return el;
         }
 
         async getSprintBacklog(boardId: string, sprintId: string): Promise<SprintBacklog> {
                 async function getIssues(startAt: number, accum: IssueValue[]): Promise<IssueValue[]> {
-                        return fetch(`/rest/agile/1.0/board/${boardId}/sprint/${sprintId}/issue?jql=issuetype in (Task, ストーリー)&startAt=${startAt}`)
-                                .then(res => res.json())
-                                .then((res: IssueResponse) => {
-                                        const isLast = res.startAt + res.maxResults >= res.total;
-                                        if (isLast) {
-                                                return accum.concat(res.issues);
-                                        }
-                                        return getIssues(res.startAt + res.maxResults, accum.concat(res.issues));
-                                });
+                        const res = await fetch(`/rest/agile/1.0/board/${boardId}/sprint/${sprintId}/issue?jql=issuetype in (Task, ストーリー)&startAt=${startAt}`);
+                        const ir: IssueResponse = await res.json();
+                        const isLast = ir.startAt + ir.maxResults >= ir.total;
+                        if (isLast) {
+                                return accum.concat(ir.issues);
+                        }
+                        return getIssues(ir.startAt + ir.maxResults, accum.concat(ir.issues));
                 }
-                return getIssues(0, [])
-                        .then(ivs => {
-                                const sbl = new SprintBacklog(sprintId, boardId);
-                                ivs.forEach(iv => {
-                                        let epicKey = null;
-                                        if (iv.fields.epic) {
-                                                epicKey = iv.fields.epic.key
-                                        }
-                                        sbl.addBacklogItem(new BacklogItem(iv.key, iv.fields.summary, epicKey));
-                                });
-                                return sbl;
-                        });
+                const ivs: IssueValue[] = await getIssues(0, []);
+                const sbl = new SprintBacklog(sprintId, boardId);
+                ivs.forEach(iv => {
+                        let epicKey = null;
+                        if (iv.fields.epic) {
+                                epicKey = iv.fields.epic.key
+                        }
+                        sbl.addBacklogItem(new BacklogItem(iv.key, iv.fields.summary, epicKey));
+                });
+                return sbl;
         }
 
         async applySprintBacklog(sprintBacklog: SprintBacklog): Promise<void> {

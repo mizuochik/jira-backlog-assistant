fbc504f25296d1731069001d2fd48ef7538596dc
Keita Mizuochi <keitam913@yahoo.co.jp>
2021-09-11 17:39:57 +0900

    Run the board

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
-let loop = () => {
-        console.log('hello JIRA');
-        setTimeout(loop, 1000);
-};
-loop();
+import { JiraBoardAccessor } from "./infra/http/jira_board_accessor";
+import { JiraBoard } from "./infra/ui/jira_board";
+import { SortSprintBacklogItemsUseCase } from "./use_case/sort_sprint_backlog_items_use_case";
+
+{
+        const a = new JiraBoardAccessor();
+        const b = new JiraBoard(a, new SortSprintBacklogItemsUseCase(a));
+        b.initialize();
+}
